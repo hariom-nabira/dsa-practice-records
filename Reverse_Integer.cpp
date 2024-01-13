e3f5fd19654705c67fe33274303bd1a713fe1db2
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-13 12:37:39 +0530

    Add Reverse Integer

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+            if( (x>0 &&  rev > (INT_MAX - (x%10))/10) || (x<0 && rev < (INT_MIN - (x%10))/10) )
+                return 0;
+            rev = rev*10 + (x%10);
+            x = x/10;
+        }
+        return rev;
+    }
+};
