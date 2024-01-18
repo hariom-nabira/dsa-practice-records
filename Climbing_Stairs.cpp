856b0b961afc2251bc404a9c3d0c32a0cc67a64e
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-18 11:41:15 +0530

    Add Climbing Stairs

--- Climbing_Stairs.cpp
+++ Climbing_Stairs.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int ans[n];
-        ans[0]=1;
-        ans[1]=2;
-        for(int i=2;i<n;i++){
-            ans[i]=ans[i-1]+ans[i-2];
-        }
-        return ans[n-1];
+        if(n<=2) return n;
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 };
-// Title: Climbing Stairs
