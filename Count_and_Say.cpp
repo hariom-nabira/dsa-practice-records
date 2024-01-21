87da62c1192efe17464880471a30366349854069
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-21 07:55:29 +0530

    Add Count and Say

--- Count_and_Say.cpp
+++ Count_and_Say.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     string countAndSay(int n) {
         if(n==1) return "1";
-        string s = countAndSay(n-1);
-        string ans;
+        string last = countAndSay(n-1);
         int count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=s[i+1]){
-                ans = ans+ to_string(count+1);
-                ans = ans+ s[i];
+        string ans;
+        for(int i=0;i<last.size();i++){
+            if(last[i]!=last[i+1]){
+                ans = ans + to_string(count+1);
+                ans = ans + (last[i]);
                 count=0;
-            }
-            else count++;
+            }else count++;
         }
         return ans;
     }
 };
-// Title: Count and Say
