131266a94abf4dcab164baa1eaee8ac6644b9c89
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-21 11:38:25 +0530

    Add House Robber

--- House_Robber.cpp
+++ House_Robber.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return helper(nums,nums.size()-1,dp);
+        return helper(nums,nums.size()-1);
     }
-    int helper(vector<int>& nums,int n,vector<int>& dp){
+    int helper(vector<int> nums,int n){
         if(n<0) return 0;
         if(n==0) return nums[0];
-        if(dp[n]==-1)
-            dp[n] =  max(nums[n]+helper(nums,n-2,dp) , helper(nums,n-1,dp));
-        return dp[n];
+        return max(nums[n]+helper(nums,n-2) , nums[n-1]+helper(nums,n-3));
     }
 };
-// Title: House Robber
