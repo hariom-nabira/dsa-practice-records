fb9b4a5640ac81915098c6e8dbd927e6a8bb7421
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-23 02:40:01 +0530

    Add Remove Duplicates from Sorted Array

--- Remove_Duplicates_from_Sorted_Array.cpp
+++ Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int last=1,n=nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[last++] = nums[i];
+        int last=1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[last] = nums[i+1];
+                last++;
             }
         }
         return last;
     }
 };
-// Title: Remove Duplicates from Sorted Array
