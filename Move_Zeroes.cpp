648f79c14932f9611d328ff17d1b3edf14cf69c9
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-26 06:30:45 +0530

    Add Move Zeroes

---
+++ Move_Zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size(),lastIndex=0,count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[lastIndex] = nums[i];
+                lastIndex++;
+            }else count++;
+        }
+        for(int i=n-1;i>n-1-count;i--)
+            nums[i]=0;
+    }
+};
