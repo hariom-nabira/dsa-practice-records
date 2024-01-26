c5cb2d9bf9ed017449d0a425e72dee98670ee85a
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-26 07:14:21 +0530

    Add Out of Boundary Paths

--- Out_of_Boundary_Paths.cpp
+++ Out_of_Boundary_Paths.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    static const int mod = 1e9+7;
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>> dp(maxMove+1,vector<vector<int>>(m,vector<int>(n,-1)));
-        return findPathsH(m,n,maxMove,startRow,startColumn,0,dp);
+        return findPathsH(m,n,maxMove,startRow,startColumn,0);
     }
-    int findPathsH(int m, int n, int maxMove, int startRow, int startColumn,int currMov,vector<vector<vector<int>>> &dp){
+    int findPathsH(int m, int n, int maxMove, int startRow, int startColumn,int currMov){
         if(currMov>maxMove) return 0;
         if(startRow<0 || startRow>=m || startColumn<0 || startColumn>=n) return 1;
-        if(dp[currMov][startRow][startColumn] == -1)
-            dp[currMov][startRow][startColumn] = (findPathsH(m,n,maxMove,startRow-1,startColumn,currMov+1,dp)%mod + findPathsH(m,n,maxMove,startRow+1,startColumn,currMov+1,dp)%mod + findPathsH(m,n,maxMove,startRow,startColumn-1,currMov+1,dp)%mod + findPathsH(m,n,maxMove,startRow,startColumn+1,currMov+1,dp)%mod )%mod;
-        return dp[currMov][startRow][startColumn];
+        return findPathsH(m,n,maxMove,startRow-1,startColumn,currMov+1) + findPathsH(m,n,maxMove,startRow+1,startColumn,currMov+1) + findPathsH(m,n,maxMove,startRow,startColumn-1,currMov+1) + findPathsH(m,n,maxMove,startRow,startColumn+1,currMov+1);
     }
 };
-// Title: Out of Boundary Paths
