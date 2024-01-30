96b91b1b5f721f49300732dd6df4d5c4892e9436
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-31 03:58:46 +0530

    Add Pow(x, n)

--- Pow(x,_n).cpp
+++ Pow(x,_n).cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==1) return x;
-        return (n>0)? x*pow(x,n-1) : 1.0/(x*pow(x,-1*(n+1)));
+        return (n>0)? x*pow(x,n-1) : 1.0/pow(x,-1*n);
     }
 };
-// Title: Pow(x, n)
