18a1cc00f63768e97dcdf8ca546680b62b97d90f
Hariom Nabira <nabira.hariom@gmail.com>
2024-01-30 18:01:34 +0530

    Add Missing Number

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sum=0,n=nums.size();
-        for(int ele : nums) sum+=ele;
-        return (n*(n+1))/2 - sum; 
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        return n*(n+1)/2 - sum;
     }
 };
-// Title: Missing Number
