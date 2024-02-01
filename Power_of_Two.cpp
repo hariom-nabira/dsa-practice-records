6142cd690185ce98b9c734d4c045f7e87b482a9d
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-01 16:23:31 +0530

    Add Power of Two

--- Power_of_Two.cpp
+++ Power_of_Two.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n && !(n&(n-1))) return true;
-        return false;
+        if(n<=0) return false;
+        return !(n&(n-1));
     }
 };
-// Title: Power of Two
