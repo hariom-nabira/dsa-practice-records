09bf5c9fedfcc0c6decb3dfe07cd907401442e2e
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-01 16:18:23 +0530

    Add Divide Array Into Arrays With Max Difference

---
+++ Divide_Array_Into_Arrays_With_Max_Difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans(nums.size()/3,vector<int> (3));
+        for(int i=0;i<nums.size();i+=3){
+            if(nums[i+1]-nums[i]<=k && nums[i+2]-nums[i+1]<=k && nums[i+2]-nums[i]<=k){
+                ans[i/3][0] = nums[i];
+                ans[i/3][1] = nums[i+1];
+                ans[i/3][2] = nums[i+2];
+            }else{
+                return vector<vector<int>>();
+            }
+        }
+        return ans;
+    }
+};
