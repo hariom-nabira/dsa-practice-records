ae8d7e991d3db0c98edb759dccabe6b32df32217
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-06 05:59:44 +0530

    Add Group Anagrams

---
+++ Group_Anagrams.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs){
+            string strcopy = s;
+            sort(strcopy.begin(),strcopy.end());
+            mp[strcopy].push_back(s);
+        }
+        for(auto pair : mp)
+            ans.push_back(pair.second);
+        return ans;
+    }
+};
