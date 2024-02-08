26db18fa7d048a0292013738166bd4f239b8c74a
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 14:44:53 +0530

    Add Sort Characters By Frequency

---
+++ Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static bool comp(pair<char,int> &p1,pair<char,int> &p2){
+        return p1.second>p2.second;
+    }
+    string frequencySort(string s) {
+        map<char,int> mp;
+        string ans="";
+        for(char ch:s)
+            mp[ch]++;
+        vector<pair<char,int>> v;
+        for(auto &pair:mp)
+            v.push_back(pair);
+        sort(v.begin(),v.end(),comp);
+        for(auto &pair:v){
+            int c=pair.second;
+            char ch=pair.first;
+            while(c--)
+                ans+=ch;
+        }
+        return ans;
+    }
+};
