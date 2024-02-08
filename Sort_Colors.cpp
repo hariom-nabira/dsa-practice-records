325205849965afa59100cbd3f46128019d93e254
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 15:35:56 +0530

    Add Sort Colors

--- Sort_Colors.cpp
+++ Sort_Colors.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int z=0,v=0,t=0;
-        for(int i=0;i<nums.size();i++)
-            if(nums[i]==0) z++;
-            else if(nums[i]==1) v++;
-            else t++;
-        int i=0;
-        while(z--) nums[i++]=0;
-        while(v--) nums[i++]=1;
-        while(t--) nums[i++]=2;
+        int left=0,right=nums.size()-1,i=0;
+        while(i<=right){
+            if(nums[i]==0){
+                swap(nums[i],nums[left]);
+                left++;i++;
+            }else if(nums[i]==2){
+                swap(nums[i],nums[right]);
+                right--;
+            }else i++;
+        }
     }
 };
-// Title: Sort Colors
