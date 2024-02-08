f6e65ff7b24dd0821732b2324818bfa1c11d3310
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 15:05:51 +0530

    Add Two Sum

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]=i;
         unordered_map<int,int>::iterator it;
         for(int i=0;i<nums.size();i++){
             it = mp.find(target-nums[i]);
-            if(it!=mp.end()) // && it->second!=i doesn't need to be checked coz in case of repeated values current occuirence not put into map yet
-                return {i,it->second};
-            mp[nums[i]]=i;
+            if(it!=mp.end() && it->second!=i)
+                    return {i,mp[target-nums[i]]};
         }
         return{-1,-1};
     }
 };
-// Title: Two Sum
