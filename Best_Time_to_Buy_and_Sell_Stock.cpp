7d2922d40b6e9c257fdf75637a5289e34becd192
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 15:42:34 +0530

    Add Best Time to Buy and Sell Stock

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minSoFar=prices[0],best=0;
+        int minsOFar=prices[0],best=0;
         for(int i=1;i<prices.size();i++){
             minSoFar = min(minSoFar,prices[i]);
             best=max(best,prices[i]-minSoFar);
@@ -9,4 +9,3 @@ public:
         return best;
     }
 };
-// Title: Best Time to Buy and Sell Stock
