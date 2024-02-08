0eb92c06f3b8c6d621c283020d1ad17e352e0d64
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 17:52:18 +0530

    Add Perfect Squares

---
+++ Perfect_Squares.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCoins(vector<int> &coins,vector<int> &dp,int v){
+        if(v==0) return 0;
+        if(dp[v]==-1){
+            int mn=INT_MAX;
+            for(int c: coins){
+                if((v-c)>=0)
+                    mn=min(mn,1+minCoins(coins,dp,v-c));
+            }
+            dp[v]=mn;
+        }
+        return dp[v];
+    }
+    int numSquares(int n) {
+        vector<int> coins,dp(n+1,-1);
+        for(int i=sqrt(n);i>=1;i--)
+            coins.push_back(i*i);
+        for(int t:coins)
+            cout<<t<<" ";
+        return minCoins(coins,dp,n);
+    }
+};
