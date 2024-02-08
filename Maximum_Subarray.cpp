bed65f95e52aa44026b6d1a9034e457c70e4e711
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-08 16:25:09 +0530

    Add Maximum Subarray

--- Maximum_Subarray.cpp
+++ Maximum_Subarray.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int mx=nums[0],sum=nums[0],i=0,j=0;
-        while(j<nums.size()-1){
+        int mx=nums[0],sum=nums[0],i=0,j=1;
+        while(j<nums.size()){
             mx=max(mx,sum);
             if(sum<0){
-                sum=nums[++j];
-                i=j;
+                sum=nums[j++];
+                i=j-1;
             }else{
-                sum+=nums[++j];
+                sum+=nums[j++];
             }
         }
         mx=max(mx,sum);
         return mx;
     }
 };
-// Title: Maximum Subarray
