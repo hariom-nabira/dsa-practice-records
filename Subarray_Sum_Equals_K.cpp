54de807a71df417ec196402214f2a597acfd8fd4
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-10 14:35:15 +0530

    Add Subarray Sum Equals K

--- Subarray_Sum_Equals_K.cpp
+++ Subarray_Sum_Equals_K.cpp
@@ -3,15 +3,12 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count=0,sum=0,temp;
         unordered_map<int,int> mp;
-        if(k) mp[0]=1;
+        mp[0]=1;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             mp[sum]++;
-            temp=mp[sum-k];
-            if(!k && sum) temp--;
-            count+=temp;
+            count+=mp[sum-k];
         }
         return count;
     }
 };
-// Title: Subarray Sum Equals K
