77b070d6509b447387b116a95206e171accb437e
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-10 16:13:15 +0530

    Add Next Permutation

--- Next_Permutation.cpp
+++ Next_Permutation.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+    void mySwap(int &a,int &b){
+        a = a^b;
+        b = a^b;
+        a = a^b;
+    }
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=n-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
-                swap(nums[i], nums[i+1]);
-                return;
-            }
+        int i=nums.size()-1;
+        while(i>0 && nums[i-1]>=nums[i]) i--;
+        if(i==0){
+            sort(nums.begin(),nums.end());
+            return;
         }
-        for(int i=0; i<n/2; i++){
-            swap(nums[i], nums[n-1-i]);
-        }
+        int j=i;
+        while(j<nums.size() && nums[j]>nums[i-1]) j++;
+        mySwap(nums[i-1],nums[j-1]);
+        sort(nums.begin()+i,nums.end());
     }
 };
-// Title: Next Permutation
