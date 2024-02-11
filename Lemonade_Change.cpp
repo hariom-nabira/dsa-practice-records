c79158fa39e0cf1402c057c39beea8733797f5c1
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-11 13:24:33 +0530

    Add Lemonade Change

---
+++ Lemonade_Change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives=0,tens=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                fives++;
+            }else if(bills[i]==10){
+                tens++;
+                if(fives==0) return false;
+                fives--;
+            }else{
+                if(fives==0 || (tens==0 && fives<3)) return false;
+                if(tens!=0){
+                    tens--;
+                    fives--;
+                }else{
+                    fives-=3;
+                }
+            }
+        }
+        return true;
+    }
+};
