f402d131c35aa487fd899c4497dd57c84dc43400
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-16 13:44:22 +0530

    Add Find First Palindromic String in the Array

---
+++ Find_First_Palindromic_String_in_the_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string str){
+        int l=0,r=str.size()-1;
+        while(l<=r){
+            if(str[l]!=str[r]) return false;
+            l++;r--;
+        }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0;i<words.size();i++)
+            if(isPalindrome(words[i])) return words[i];
+        return "";
+    }
+};
