05db311f58270222cfd09c4e06af4a56976d6bd7
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-16 13:39:56 +0530

    Add Least Number of Unique Integers after K Removals

---
+++ Least_Number_of_Unique_Integers_after_K_Removals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int,int> mp;
+        for(int it:arr) mp[it]++;
+        vector<int> v;
+        for(auto p:mp) v.push_back(p.second);
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            if(k>=v[i]) k-=v[i];
+            else return v.size()-i;
+        }
+        return 0;
+    }
+};
