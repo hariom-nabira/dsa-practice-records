435de1ea40b9e036762f8c9cb5c6bf7251ebf743
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-16 13:23:39 +0530

    Add Find Polygon With the Largest Perimeter

---
+++ Find_Polygon_With_the_Largest_Perimeter.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        long long s=0;
+        for(int it:nums) s+=it;
+        for(int i=nums.size()-1;i>=0;i--){
+            if((s-nums[i]) > nums[i])
+                return s;
+            s-=nums[i];
+        }
+        return -1;
+    }
+};
