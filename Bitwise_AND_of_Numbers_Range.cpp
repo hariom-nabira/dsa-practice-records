0aa21d3fb7f889118f66e859e4defa018dbdc636
Hariom Nabira <nabira.hariom@gmail.com>
2024-02-21 04:52:30 +0530

    Add Bitwise AND of Numbers Range

--- Bitwise_AND_of_Numbers_Range.cpp
+++ Bitwise_AND_of_Numbers_Range.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int count=0;
-        while(left!=right){
+        int leftc=left,rightc=right,count=-1;
+        while(left>0 && right>0){
             count++;
             left>>=1;
             right>>=1;
         }
-        if(!left) return 0;
-        return left<<count;
+        if(!left && right) return 0;
+        if(!right && left) return 0;
+        int ans = (count>-1)? (1<<count) : 0;
+        int temp = leftc ^ rightc;
+        while(count >= 0){
+            cout<<temp<<" "<<count<<" "<<(1<<count)<<" ";
+            if ((temp & (1<<count))) {
+                break;
+            }
+            ans = ans | (leftc&(1<<count));
+            cout<<ans<<endl;
+            count--;
+        }
+        return ans;
     }
 };
-// Title: Bitwise AND of Numbers Range
