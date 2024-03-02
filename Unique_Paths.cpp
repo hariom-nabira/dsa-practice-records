9f1a63196b44ce0ad8b3182a32b085a81622f7b0
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-02 14:01:44 +0530

    Add Unique Paths

--- Unique_Paths.cpp
+++ Unique_Paths.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long ans=1;
-        for(int i=m+n-2, j=1; i >= max(m,n);i--, j++)
-            ans = (ans*i)/j;
-        return ans;
+        long long numerator=1,denominator=1;
+        for(int i=m+n-2; i>m-1 && i>n-1;i--)
+            numerator *= i;
+        for(int i=2;i<m && i<n;i++)
+            denominator *= i;
+        return numerator/denominator;
     }
 };
-// Title: Unique Paths
