a84477254a844fccd73340caa00b94cff43e6194
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-02 06:27:50 +0530

    Add Squares of a Sorted Array

---
+++ Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        int left = n-1,right = n;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+                left=i-1;
+                right=i;
+                break;
+            }
+        }
+        while(left>=0 && right<n){
+            if(-1*nums[left] < nums[right]){
+                ans.push_back(nums[left]*nums[left]);
+                left--;
+            }else{
+                ans.push_back(nums[right]*nums[right]);
+                right++;
+            }
+        }
+        while(left>=0){
+            ans.push_back(nums[left]*nums[left]);
+            left--;
+        }
+        while(right<n){
+            ans.push_back(nums[right]*nums[right]);
+            right++;
+        }
+        return ans;
+    }
+};
