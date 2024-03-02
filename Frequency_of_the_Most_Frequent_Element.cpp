d7675c1a8b12d1c653e6e932c5cdeb77e94d9047
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-02 12:17:41 +0530

    Add Frequency of the Most Frequent Element

--- Frequency_of_the_Most_Frequent_Element.cpp
+++ Frequency_of_the_Most_Frequent_Element.cpp
@@ -1,4 +1,4 @@
-// Sliding Window Approach (from editorial approach 2)
+// Sliding Window Approach (from editorial)
 // We take a Sorted Array and a target element from that array, now we want to use at most k increments to maximise freq of target
 // since increments, we can only play with elements on left
 // Let window be all elements we have taken to be incremented to target
@@ -13,16 +13,12 @@ public:
         for(int right=0;right<n;right++){ //we consider each element as target once
             target=nums[right];
             currSum+=nums[right];
-
-            // CATCH - Since we only care about the max length of window and not the actual window
-            //         wherever we need to shrink to make window valid we can shrink once and  move to the next element (essentialy making no change to the length of window)
-
-            if(1LL*(right-left+1)*target - currSum > k){ //if total operation is more than k then we need to let go of farthest(i.e left) element
+            while(1LL*(right-left+1)*target - currSum > k){ //if total operation is more than k then we need to let go of farthest(i.e left) element
                 currSum-=nums[left];
                 left++;
             }
+            bestLength = max(right-left+1 ,bestLength);
         }
-        return n-left;
+        return bestLength;
     }
 };
-// Title: Frequency of the Most Frequent Element
