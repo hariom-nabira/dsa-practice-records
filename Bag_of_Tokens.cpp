57054ee2e318fcb2ab996ce7cca33b887e5debeb
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-04 15:31:51 +0530

    Add Bag of Tokens

---
+++ Bag_of_Tokens.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+        int score=0,left=0,right=tokens.size()-1,maxScore = 0;
+        while(left<=right){
+            if(tokens[left] > power){
+                if(score==0) break;
+                power+=tokens[right];
+                score--;
+                right--;
+            }else{
+                power-=tokens[left];
+                score++;
+                left++;
+            }
+            maxScore = max(maxScore,score);
+        }
+        return maxScore;
+    }
+};
