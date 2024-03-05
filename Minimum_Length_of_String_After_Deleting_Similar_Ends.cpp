7c0ba292e308098f44b434d2da33d62fc144c9d7
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-05 12:57:40 +0530

    Add Minimum Length of String After Deleting Similar Ends

---
+++ Minimum_Length_of_String_After_Deleting_Similar_Ends.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n = s.size();
+        int left=0, right = n-1;
+        char ch;
+        while(left<right && s[left]==s[right]){
+            ch = s[left];
+            while(s[left]==ch && left<=right)
+                left++;
+            while(s[right]==ch && right>=left)
+                right--;
+        }
+        return right-left+1;
+    }
+};
