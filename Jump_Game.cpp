a0c9fc5b9e841a08924340dc7b342e24cf27a8ec
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-06 18:42:47 +0530

    Add Jump Game

--- Jump_Game.cpp
+++ Jump_Game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        for(int i=nums.size()-2;i>=0;i--){
+        for(int i=nums.size()-1;i>=0;i--){
             if(nums[i]==0){
                 bool flag = true;
                 for(int j=i-1;j>=0;j--){
@@ -17,4 +17,3 @@ public:
         return true;
     }
 };
-// Title: Jump Game
