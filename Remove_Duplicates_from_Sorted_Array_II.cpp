a1c0adf2e00263adacf439b5e9e9ee37add758a2
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-10 06:20:06 +0530

    Add Remove Duplicates from Sorted Array II

--- Remove_Duplicates_from_Sorted_Array_II.cpp
+++ Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -1,16 +1,22 @@
-// solutions me se uthai hui smart approach
-
 class Solution {
- public:
-  int removeDuplicates(vector<int>& nums) {
-    int i = 0;
-
-    for (const int num : nums)
-      if (i < 2 || num > nums[i - 2]) // i<2 kyuki pehle 2 elements to aaenge hi
-        nums[i++] = num;              // i is always 1 ahead of accepted array,
-                                      // therefore if nums[i-2]==num(current value being judged)
-                                      // that means num ke 2 already ho gye
-    return i;
-  }
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=2) return n;
+        int left=1,right=2,count=2;
+        while(right<n){
+            if(nums[right]!=nums[left]){
+                count++;
+                nums[++left]=nums[right++];
+            }else{
+                if(nums[left-1]!=nums[left]){
+                    count++;
+                    nums[++left]=nums[right];
+                }
+                right++;
+            }
+        }
+        cout<<count<<endl;
+        return count;
+    }
 };
-// Title: Remove Duplicates from Sorted Array II
