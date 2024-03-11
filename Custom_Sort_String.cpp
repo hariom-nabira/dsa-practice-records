597fedbc0a0a6035287f0d8d007b8c36500d90f7
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-11 07:16:04 +0530

    Add Custom Sort String

---
+++ Custom_Sort_String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> freq(26,0);
+        string ans;
+        for(const char ch : s){
+            freq[ch-'a']++;
+        }
+        for(const char ch : order){
+            for(int i=0;i<freq[ch-'a'];i++){
+                ans+=ch;
+            }
+            freq[ch-'a'] = 0;
+        }
+        for(int i=0;i<26;i++){
+            for(int j=0;j<freq[i];j++){
+                ans+=  (char)('a'+i);
+            }
+        }
+        return ans;
+    }
+};
