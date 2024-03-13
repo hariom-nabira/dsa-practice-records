d9bda11a77a9e6b8e87d171252dfd3eab51d304d
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-14 03:33:45 +0530

    Add Binary Subarrays With Sum

---
+++ Binary_Subarrays_With_Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int ans=0,currSum=0;
+        for(int i=0;i<nums.size();i++){
+            currSum+=nums[i];
+            if(mp.count(currSum-goal))
+                ans+=mp[currSum-goal];
+            mp[currSum]++;
+        }
+        return ans;
+    }
+};
