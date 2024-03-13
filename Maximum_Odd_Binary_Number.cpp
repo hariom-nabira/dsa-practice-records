ec975a9ea14460381787861bc03e592a5f444742
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-14 03:38:09 +0530

    Add Maximum Odd Binary Number

---
+++ Maximum_Odd_Binary_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int count = 0;
+        for(char ch:s)
+            if(ch=='1') count++;
+        string ans(count-1,'1');
+        for(int i=0;i<s.size()-count;i++)
+            ans.push_back('0');
+        ans.push_back('1');
+        return ans;
+    }
+};
