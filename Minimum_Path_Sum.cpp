8307d9c639f81fb9dffa46bb49934b6fa253318c
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-16 04:41:38 +0530

    Add Minimum Path Sum

--- Minimum_Path_Sum.cpp
+++ Minimum_Path_Sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> dp(grid);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int mn = INT_MAX;
                 if(i>0)
-                    mn = min(mn,grid[i-1][j]);
+                    mn = min(mn,dp[i-1][j]);
                 if(j>0)
-                    mn = min(mn,grid[i][j-1]);
-                grid[i][j] += (mn==INT_MAX)?0:mn;
+                    mn = min(mn,dp[i][j-1]);
+                dp[i][j] += (mn==INT_MAX)?0:mn;
             }
         }
-        return grid[n-1][m-1];
+        return dp[n-1][m-1];
     }
 };
-// Title: Minimum Path Sum
