616a2e76a24fb926aa8b30fdda92ea2334d14a16
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-16 03:43:16 +0530

    Add Contiguous Array

--- Contiguous_Array.cpp
+++ Contiguous_Array.cpp
@@ -3,7 +3,7 @@ public:
     int findMaxLength(vector<int>& nums) {
         unordered_map<int,int> mp;
         mp[0]=-1;
-        int sum=0, ans = 0;
+        int sum=0, ans = INT_MIN;
         for(int i=0;i<nums.size();i++){
             if(nums[i]&1) sum++;
             else sum--;
@@ -14,4 +14,3 @@ public:
         return ans;
     }
 };
-// Title: Contiguous Array
