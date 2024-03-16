8ede7b3252b4f53504635bfaf6cf97249f5000bf
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-17 03:06:06 +0530

    Add Existence of a Substring in a String and Its Reverse

---
+++ Existence_of_a_Substring_in_a_String_and_Its_Reverse.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubstringPresent(string s) {
+        if(s.size()<2) return false;
+        set<string> s1,s2;
+        for(int i=0;i<s.size()-1;i++){
+            s1.insert(s.substr(i,2));
+        }
+        for(int j=s.size()-1;j>0;j--){
+            string str = "";
+            str += s[j];
+            str += s[j-1];
+            s2.insert(str);
+        }
+        for(auto str:s1)
+            if(s2.contains(str))
+               return true;
+        return false;
+    }
+};
