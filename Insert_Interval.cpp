47ed1a6267ac25291fd6a9cb173c80aed4848a50
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-17 15:47:50 +0530

    Add Insert Interval

--- Insert_Interval.cpp
+++ Insert_Interval.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size();
-        if(n==0) return {newInterval};
         vector<vector<int>> ans;
+        if(intervals.size()==0){
+            return {newInterval};
+        }
         int i=0;
-        while(i<n && intervals[i][1] < newInterval[0]){
+        while(intervals[i][1] < newInterval[0]){
             ans.push_back(intervals[i]);
             i++;
         }
-        if(i == n){
-            ans.push_back(newInterval);
-            return ans;
-        }
+
         int startTime = min(intervals[i][0],newInterval[0]);
         int bookedTill = max(intervals[i][1],newInterval[1]);
         i++;
 
-        while(i<n && bookedTill >= intervals[i][0]){
+        while(i<intervals.size() && bookedTill >= intervals[i][0]){
             bookedTill = max(intervals[i][1],bookedTill);
             i++;
         }
         ans.push_back({startTime,bookedTill});
-        while(i<n){
+        while(i<intervals.size()){
             ans.push_back(intervals[i]);
             i++;
         }
         return ans;
     }
 };
-// Title: Insert Interval
