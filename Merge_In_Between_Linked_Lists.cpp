ddf7d8f00440beb60db9e52c706aa54dce73da9d
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-20 03:55:49 +0530

    Add Merge In Between Linked Lists

---
+++ Merge_In_Between_Linked_Lists.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode *temp = list1, *temp2;
+        int i=0;
+        while(i++ < a-1){
+            temp = temp->next;
+        }
+        temp2 = temp->next;
+        temp->next = list2;
+
+        while(i++ <= b)
+            temp2 = temp2->next;
+            
+        while(list2->next!=NULL)
+            list2 = list2->next;
+        list2->next = temp2;
+        return list1;
+    }
+};
