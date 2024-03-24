a60e2d5ae53bffda7244d23bf83a7bec49370098
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-24 19:14:11 +0530

    Add Find the Duplicate Number

--- Find_the_Duplicate_Number.cpp
+++ Find_the_Duplicate_Number.cpp
@@ -1,19 +1,26 @@
+// chapa for streak
+// if programming was an anime - yt video lol
+
+
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        int slow = nums[0], fast = nums[0];
-        do{
+    int findDuplicate(std::vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find the intersection point of the two pointers
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-            cout<<slow<<">>>>>>>>"<<fast;
-        }while(slow!=fast);
-        int temp = nums[0];
-        while(slow!=temp){
-            cout<<slow<<">>>>>>>>"<<temp;
+        } while (slow != fast);
+
+        // Find the entrance of the cycle
+        slow = nums[0];
+        while (slow != fast) {
             slow = nums[slow];
-            temp = nums[temp];
+            fast = nums[fast];
         }
-        return temp;
+
+        return slow;
     }
 };
-// Title: Find the Duplicate Number
