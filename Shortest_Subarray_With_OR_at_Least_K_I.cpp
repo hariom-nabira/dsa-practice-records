e99c9de1a9ee0dc8217888e686a52641b776d83b
Hariom Nabira <nabira.hariom@gmail.com>
2024-03-30 15:05:29 +0530

    Add Shortest Subarray With OR at Least K I

---
+++ Shortest_Subarray_With_OR_at_Least_K_I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int mn = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            int xr=0;
+            for(int j=i;j<nums.size();j++){
+                xr = xr | nums[j];
+                if(xr>=k){
+                    mn = min(mn,j-i+1);
+                    break;
+                }
+            }
+        }
+        if(mn==INT_MAX)return -1;
+        return mn;
+    }
+};
