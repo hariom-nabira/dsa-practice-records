ab67aa2c29d27d2c8d8b4b255d0d4c2ade010640
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-03 16:41:07 +0530

    Add Word Search

---
+++ Word_Search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>> &board, string word) {
+        int m = board.size(), n = board[0].size(); 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(helper(board,i,j,0,word))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    bool helper(vector<vector<char>> &board,int i,int j,int k,string &word){
+        if(k==word.size()) return true;
+        int m = board.size(), n = board[0].size(); 
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[k]){
+            return false;
+        }
+        board[i][j] = '@';
+        if( helper(board,i-1,j,k+1,word) || helper(board,i+1,j,k+1,word) ||
+            helper(board,i,j-1,k+1,word) || helper(board,i,j+1,k+1,word) )
+                return true;
+        board[i][j]=word[k];
+        return false;;
+    }
+};
