ee09b9c311a715c9625d42f776aad1bcbbdfbd02
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-08 11:48:31 +0530

    Add Number of Students Unable to Eat Lunch

---
+++ Number_of_Students_Unable_to_Eat_Lunch.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int n = students.size();
+        int freqStu[2];
+        for(int ele : students)
+            freqStu[ele]++;
+        for(int i=0; i<n; i++)
+            if(freqStu[sandwiches[i]]>0)
+                freqStu[sandwiches[i]]--;
+            else
+                return n-i;
+        return 0;
+    }
+};
