4674d1b61932d7d2fafeac8f6fe5a7f665fb3963
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-09 09:16:57 +0530

    Add Time Needed to Buy Tickets

---
+++ Time_Needed_to_Buy_Tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=tickets[k];
+        for(int i=0;i<k;i++)
+            if(tickets[i] >= tickets[k])
+                ans+=tickets[k];
+            else
+                ans+= tickets[i];
+        for(int i=k+1; i<tickets.size();i++)
+            if(tickets[i] >= tickets[k])
+                ans += tickets[k] - 1;
+            else
+                ans+= tickets[i];
+        return ans;
+    }
+};
