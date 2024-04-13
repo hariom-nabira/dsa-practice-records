7d49d698b9e3d6fdbe23162eb9d5db6ee1175fd2
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-14 03:17:56 +0530

    Add Kth Smallest Amount With Single Denomination Combination

---
+++ Kth_Smallest_Amount_With_Single_Denomination_Combination.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long findKthSmallest(vector<int>& coins, int k) {
+        int minCoin = INT_MAX;
+        for(int ele: coins)
+            minCoin = min(minCoin,ele);
+        long long range = 1LL*k*minCoin;
+        set<long long> amounts;
+        for(int ele:coins){
+            int i=1;
+            while(1LL*ele*i <= range){
+                amounts.insert(1LL*ele*i);
+                i++;
+            }
+        }
+        auto itr = amounts.begin();
+        for(int i=1;i<k;i++) itr++;
+        return *itr;
+    }
+};
