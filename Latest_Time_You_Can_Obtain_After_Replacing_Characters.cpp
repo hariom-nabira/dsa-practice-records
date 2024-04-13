6e87629406c8b94c73c2399c52b2ba8b1d2797d4
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-14 02:36:02 +0530

    Add Latest Time You Can Obtain After Replacing Characters

--- Latest_Time_You_Can_Obtain_After_Replacing_Characters.cpp
+++ Latest_Time_You_Can_Obtain_After_Replacing_Characters.cpp
@@ -3,7 +3,7 @@ public:
     string findLatestTime(string s) {
         for(int i=0;i<5;i++){
             if(s[i]=='?'){
-                if(i==0) s[i] = (s[i+1] <='0' || s[i+1]=='?') ? '1' : '0';
+                if(i==0) s[i] = '1';
                 else if(i==1) s[i] = (s[i-1]=='0')? '9' : '1';
                 else if(i==3) s[i] = '5';
                 else s[i] = '9';
@@ -12,4 +12,3 @@ public:
         return s;
     }
 };
-// Title: Latest Time You Can Obtain After Replacing Characters
