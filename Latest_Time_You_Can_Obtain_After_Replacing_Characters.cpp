c6a777f0d5906017bf80bf621eead8bf8d8fc9fc
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-14 02:38:27 +0530

    Add Latest Time You Can Obtain After Replacing Characters

---
+++ Latest_Time_You_Can_Obtain_After_Replacing_Characters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string findLatestTime(string s) {
+        for(int i=0;i<5;i++){
+            if(s[i]=='?'){
+                if(i==0) s[i] = (s[i+1] <='1' || s[i+1]=='?') ? '1' : '0';
+                else if(i==1) s[i] = (s[i-1]=='0')? '9' : '1';
+                else if(i==3) s[i] = '5';
+                else s[i] = '9';
+            }
+        }
+        return s;
+    }
+};
