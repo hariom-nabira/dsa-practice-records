0f6a2442812d234b76e9646ed30893c6085f504b
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-15 00:34:44 +0530

    Add Sum Root to Leaf Numbers

---
+++ Sum_Root_to_Leaf_Numbers.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return helper(root,0);
+    }
+    int helper(TreeNode* root, int parentVal){
+        if(root==NULL)
+            return 0;
+        if(root->left==NULL && root->right==NULL)
+            return parentVal+root->val;
+        parentVal += root->val;
+        return helper(root->left, parentVal*10) + helper(root->right, parentVal*10);
+    }
+};
