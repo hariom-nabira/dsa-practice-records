362e9fb7e19ef5f0d4b5fbc59013b029c5780204
Hariom Nabira <nabira.hariom@gmail.com>
2024-04-17 10:43:11 +0530

    Add Smallest String Starting From Leaf

---
+++ Smallest_String_Starting_From_Leaf.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        return helper(root,"");
+    }
+    string helper(TreeNode* root, string soFar){
+        if(root->left==NULL && root->right==NULL){
+            reverse(soFar.begin(),soFar.end());
+            return (char)('a'+root->val) + soFar;
+        }
+        string l, r;
+        if(root->left) l = helper(root->left, soFar+(char)('a'+root->val));
+        if(root->right) r = helper(root->right, soFar+(char)('a'+root->val));
+        
+        if(l!="" && r!="")
+            return min(l,r);
+        else if(l!="")
+            return l;
+        else
+            return r;
+    }
+};
