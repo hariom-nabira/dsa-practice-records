078a6f108c51ca554b07acca62fc20ac80fb5e71
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-10 05:12:12 +0530

    Add Maximize Happiness of Selected Children

--- Maximize_Happiness_of_Selected_Children.cpp
+++ Maximize_Happiness_of_Selected_Children.cpp
@@ -3,13 +3,8 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         long long ans=0;
         sort(happiness.begin(),happiness.end(),greater<int>());
-        for(int i=0;i<k;i++){
-            if(happiness[i]>i)
-                ans+=happiness[i]-i;
-            else
-                break;
-        }
+        for(int i=0;i<k;i++)
+            ans += max(happiness[i]-i, 0);
         return ans;
     }
 };
-// Title: Maximize Happiness of Selected Children
