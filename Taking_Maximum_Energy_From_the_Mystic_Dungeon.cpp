795ff5e2c71dacc107b729d0eb95cddb1a346c86
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-12 03:29:56 +0530

    Add Taking Maximum Energy From the Mystic Dungeon

---
+++ Taking_Maximum_Energy_From_the_Mystic_Dungeon.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int ans=INT_MIN, n = energy.size();
+        for(int i=0;i<k;i++){
+            int last = n-1-((n-i-1)%k);
+            int sum = energy[last], currBest = energy[last];
+            for(int j=last-k;j>=i;j-=k){
+                sum+=energy[j];
+                currBest = max(sum,currBest);
+            }
+            ans = max(ans,currBest);
+        }
+        return ans;
+    }
+};
