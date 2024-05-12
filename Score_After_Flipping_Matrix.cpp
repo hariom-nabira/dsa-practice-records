9208d219732283ee1ad5f6f8950b76f3b5c06ef0
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-13 05:21:34 +0530

    Add Score After Flipping Matrix

--- Score_After_Flipping_Matrix.cpp
+++ Score_After_Flipping_Matrix.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size(), ans=0;
+        for(int i=0;i<n;i++){
+            if(grid[i][0]==1) continue;
+            for(int j=0;j<m;j++){
+                grid[i][j] = (grid[i][j]==0)?1:0;
+            }
+        }
         ans += n*(1<<(m-1));
         for(int j=1;j<m;j++){
-            int ones=0;
+            int count=0;
             for(int i=0;i<n;i++){
-                if(grid[i][j]==grid[i][0]) ones++;
+                if(grid[i][j]==1) count++;
             }
-            if(ones>=(n+1)/2)
-                ans += ones*(1<<(m-1-j));
+            if(count>=(n+1)/2)
+                ans += count*(1<<(m-1-j));
             else
-                ans += (n-ones)*(1<<(m-1-j));
+                ans += (n-count)*(1<<(m-1-j));
         }
         return ans;
     }
 };
-// Title: Score After Flipping Matrix
