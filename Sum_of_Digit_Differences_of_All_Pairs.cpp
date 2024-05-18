a576c3100edb3e4c4c7ca421eeae4c5d4c8d92b9
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-19 03:54:19 +0530

    Add Sum of Digit Differences of All Pairs

--- Sum_of_Digit_Differences_of_All_Pairs.cpp
+++ Sum_of_Digit_Differences_of_All_Pairs.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     long long sumDigitDifferences(vector<int>& nums) {
         int n=nums.size();
-        long long temp = nums[0],digits=0;
+        int temp = nums[0],digits=0;
         while(temp!=0){
             digits++;
             temp/=10;
         }
-        vector<vector<long long>> data(digits,vector<long long>(10,0));
+        vector<vector<int>> data(digits,vector<int>(10,0));
         for(int i=0;i<n;i++){
             temp = nums[i];
-            for(int d=0;d<digits;d++){
+            int d=0;
+            for(d=0;d<digits;d++){
                 data[d][temp%10]++;
                 temp/=10;
             }
         }
-        long long ans=0;
+        int ans=0;
         for(int i=0;i<digits;i++){
             int d=n;
             for(int j=0;j<9;j++){
@@ -26,4 +27,3 @@ public:
         return ans;
     }
 };
-// Title: Sum of Digit Differences of All Pairs
