9c65da83001521e55f10729306b94ca4130d1b59
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-20 05:04:20 +0530

    Add Sum of All Subset XOR Totals

---
+++ Sum_of_All_Subset_XOR_Totals.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return helper(nums,0,0);
+    }
+    int helper(vector<int>& nums, int i,int xr){
+        if(i==nums.size()) return xr;
+        return helper(nums,i+1,xr^nums[i]) + helper(nums,i+1,xr);
+    }
+};
