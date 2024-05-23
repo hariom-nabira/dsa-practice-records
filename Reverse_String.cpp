65d7f386f2ad78fd223248552ad82a263f2da1cd
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-23 09:20:56 +0530

    Add Reverse String

--- Reverse_String.cpp
+++ Reverse_String.cpp
@@ -4,11 +4,10 @@ public:
         helper(s,0,s.size()-1);
     }
     void helper(vector<char> &s, int start, int end){
-        if(start>end) return;
+        if(start<end) return;
         char temp = s[start];
         s[start] = s[end];
         s[end] = temp;
         helper(s,start+1,end-1);
     }
 };
-// Title: Reverse String
