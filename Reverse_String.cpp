05d2879831e9a2a60cc76200560c9eb8a39e731b
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-23 10:15:03 +0530

    Add Reverse String

--- Reverse_String.cpp
+++ Reverse_String.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int start = 0, end = s.size()-1;
-        while(start<end){
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
-        }
+        helper(s,0,s.size()-1);
+    }
+    void helper(vector<char> &s, int start, int end){
+        if(start>end) return;
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        helper(s,start+1,end-1);
     }
 };
-// Title: Reverse String
