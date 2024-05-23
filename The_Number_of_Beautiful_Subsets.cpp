dd27db15d732bef981411ac8b2b806707c71d208
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-23 07:05:47 +0530

    Add The Number of Beautiful Subsets

--- The_Number_of_Beautiful_Subsets.cpp
+++ The_Number_of_Beautiful_Subsets.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
         int ans=0;
-        unordered_map<int,int> path;
+        unordered_set<int> path;
         backtrack(nums,k,0,path,ans);
         return  ans;
     }
-    void backtrack(vector<int> &nums, int k, int index, unordered_map<int,int> &path, int &ans){
+    void backtrack(vector<int> &nums, int k, int index, unordered_set<int> &path, int &ans){
         if(index==nums.size()) return;
-        if(path[nums[index]-k]==0 && path[nums[index]+k]==0){
-            path[nums[index]]++;
+        if(!path.contains(nums[index]-k) && !path.contains(nums[index]+k)){
+            path.insert(nums[index]);
             ans++;
             backtrack(nums,k,index+1,path,ans);
-            path[nums[index]]--;
+            path.erase(nums[index]);
         }
         backtrack(nums,k,index+1,path,ans);
     }
 };
-// Title: The Number of Beautiful Subsets
