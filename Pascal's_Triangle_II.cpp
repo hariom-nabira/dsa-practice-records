60344fb69f62fab26c07336334716bb71a15c9e3
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-24 08:14:37 +0530

    Add Pascal's Triangle II

---
+++ Pascal's_Triangle_II.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans(rowIndex+1, 1);
+        for(int k=1;k<ans.size();k++){
+            ans[k] = (1LL*ans[k-1]*(rowIndex-k+1))/(k);
+        }
+        return ans;
+    }
+};
