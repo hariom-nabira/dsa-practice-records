762d24c49fe405ba1f17ff8548ed20256a57f3b6
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-26 02:51:42 +0530

    Add Find the Number of Good Pairs II

--- Find_the_Number_of_Good_Pairs_II.cpp
+++ Find_the_Number_of_Good_Pairs_II.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         long long ans=0;
-        sort(nums1.begin(),nums1.end(), greater<int>());
+        sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         for(int i=0;i<nums1.size();i++){
-            if(nums1[i] < (nums2[0]*k))
-                break;
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i] < (nums2[j]*k))
                     break;
@@ -16,4 +14,3 @@ public:
         return ans;
     }
 };
-// Title: Find the Number of Good Pairs II
