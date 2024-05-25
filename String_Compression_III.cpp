4058bf7864aa814bd29092648e2a811bc5248dd2
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-26 02:46:32 +0530

    Add String Compression III

---
+++ String_Compression_III.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string ans;
+        int i=0, n = word.size();
+        while(i < n){
+            int count=1;
+            char prev = word[i];
+            while(i<(n-1) && word[i+1]==prev && count<9){
+                count++;
+                i++;
+            }
+            ans += ('0'+ count);
+            ans += prev;
+            i++;
+        }
+        return ans;
+    }
+};
