7a59471b4e16d6c05a82801efd25dfe2966b04dc
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-26 16:44:58 +0530

    Add Student Attendance Record II

--- Student_Attendance_Record_II.cpp
+++ Student_Attendance_Record_II.cpp
@@ -1,32 +1,43 @@
+//copied for streak
+
 class Solution {
-    int MOD = 1e9+7;
+    int MOD = 1000000007;
+    // Cache to store sub-problem results.
+    vector<vector<vector<int>>> memo;
 
 public:
-    int eligibleCombinations(int n, int totalAbsences, int consecutiveLates, vector<vector<vector<int>>> &memo) {
+    // Recursive function to return the count of combinations of length 'n' eligible for the award.
+    int eligibleCombinations(int n, int totalAbsences, int consecutiveLates) {
+        // If the combination has become not eligible for the award,
+        // then we will not count any combinations that can be made using it.
         if (totalAbsences >= 2 or consecutiveLates >= 3) {
             return 0;
         }
+        // If we have generated a combination of length 'n' we will count it.
         if (n == 0) {
             return 1;
         }
+        // If we have already seen this sub-problem earlier, we return the stored result.
         if (memo[n][totalAbsences][consecutiveLates] != -1) {
             return memo[n][totalAbsences][consecutiveLates];
         }
+
         int count = 0;
         // We choose 'P' for the current position.
-        count = eligibleCombinations(n - 1, totalAbsences, 0, memo);
+        count = eligibleCombinations(n - 1, totalAbsences, 0);
         // We choose 'A' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0, memo)) % MOD;
+        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0)) % MOD;
         // We choose 'L' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1, memo)) % MOD;
+        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1)) % MOD;
 
         // Return and store the current sub-problem result in the cache.
         return memo[n][totalAbsences][consecutiveLates] = count;
     }
 
     int checkRecord(int n) {
-        vector<vector<vector<int>>> memo(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        return eligibleCombinations(n, 0, 0, memo);
+        // Initialize the cache.
+        memo = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        // Return count of combinations of length 'n' eligible for the award.
+        return eligibleCombinations(n, 0, 0);
     }
 };
-// Title: Student Attendance Record II
