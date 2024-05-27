2fe4a9fe2a99e1a9a3375662eddb758a9a36a113
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-27 18:10:50 +0530

    Add To Be Or Not To Be

---
+++ To_Be_Or_Not_To_Be.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let obj = {
+        toBe: (arg) => {
+            if(val===arg)
+                return true;
+            else
+                throw new Error("Not Equal");
+        },
+        notToBe: (arg)=> {
+            if(val!==arg)
+                return true;
+            else
+                throw new Error("Equal");
+        }
+    };
+    return obj;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
