89e1288d118d316394da99668617a544d64b6752
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-28 06:54:45 +0530

    Add Return Length of Arguments Passed

--- Return_Length_of_Arguments_Passed.js
+++ Return_Length_of_Arguments_Passed.js
@@ -1,6 +1,11 @@
-
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
 var argumentsLength = function(...args) {
     return args.length;
 };
 
-// Title: Return Length of Arguments Passed
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
