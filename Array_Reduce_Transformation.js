2a6295a9082a178cca7ba31225091f7f5a55e73c
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-28 05:34:21 +0530

    Add Array Reduce Transformation

---
+++ Array_Reduce_Transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    return nums.reduce((prev,ele,index)=>{
+        return fn(prev,ele);
+    },init);
+};
