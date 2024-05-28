9b21c4d0966d22ec7a9d88d322782f64e647aa81
Hariom Nabira <nabira.hariom@gmail.com>
2024-05-28 06:28:24 +0530

    Add Function Composition

--- Function_Composition.js
+++ Function_Composition.js
@@ -5,9 +5,10 @@
 var compose = function(functions) {
     
     return function(x) {
-        return functions.reduceRight((prev,fn)=>{
-            return fn(prev);
-        },x);
+        for(let i=functions.length-1; i>=0; i--){
+            x = functions[i](x);
+        }
+        return x;
     };
 };
 
@@ -15,4 +16,3 @@ var compose = function(functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
-// Title: Function Composition
