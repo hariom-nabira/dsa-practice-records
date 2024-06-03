b457a986fdaef809ddf548e9e8807d4ad4a9d595
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-03 08:59:15 +0530

    Add Append Characters to String to Make Subsequence

--- Append_Characters_to_String_to_Make_Subsequence.cpp
+++ Append_Characters_to_String_to_Make_Subsequence.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        int j=0,n=s.size();
-        for(int i=0;i<n;i++){
+        int j=0;
+        for(int i=0;i<s.size();i++){
             if(s[i]==t[j]) j++;
         }
         return t.size()-j;
     }
 };
-// Title: Append Characters to String to Make Subsequence
