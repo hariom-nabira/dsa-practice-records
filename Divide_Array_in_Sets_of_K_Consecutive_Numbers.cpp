52be6c11677f98a2bd86ff468e40633094f571cf
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-06 16:11:39 +0530

    Add Divide Array in Sets of K Consecutive Numbers

---
+++ Divide_Array_in_Sets_of_K_Consecutive_Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& hand, int groupSize) {
+        if(hand.size()%groupSize!=0) return false;
+        map<int,int> freq;
+        for(auto &ele:hand) freq[ele]++;
+        while(!freq.empty()){
+            int start = freq.begin()->first;
+            int need = freq.begin()->second;
+            for(int i=0; i<groupSize;i++){
+                if(freq[start+i] < need) return false;
+                freq[start+i] -= need;
+                if(freq[start+i]==0) freq.erase(start+i);
+            }
+            freq.erase(start);
+        }
+        return true;
+    }
+};
