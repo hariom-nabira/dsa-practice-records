580065bacbd51cf8354f7b1814a36562ed9780a6
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-09 02:55:11 +0530

    Add Find the N-th Value After K Seconds

---
+++ Find_the_N-th_Value_After_K_Seconds.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int valueAfterKSeconds(int n, int k) {
+        vector<long long int> a(n,1);
+        for(int i=0;i<k;i++){
+            for(int j=1;j<n;j++){
+                a[j] = (a[j]+a[j-1])%MOD;
+            }
+        }
+        return a[n-1];
+    }
+};
