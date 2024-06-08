8b5ebf19fbffcdc00e223217bf71835c7450bdf3
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-08 12:39:02 +0530

    Add Replace Words

--- Replace_Words.cpp
+++ Replace_Words.cpp
@@ -1,31 +1,80 @@
-class Solution {
+class TrieNode {
 public:
-    vector<string> split(string sentence, char delimiter){
-        stringstream ss(sentence);
-        string token;
-        vector<string> res;
-        while(getline(ss,token,delimiter))
-            res.push_back(token);
-        return res;
+    bool isEnd;
+    vector<TrieNode*> children;
+
+    TrieNode() : children(26, nullptr) { isEnd = false; }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() { root = new TrieNode(); }
+
+    void insert(string word) {
+        TrieNode* current = root;
+        for (char c : word) {
+            if (current->children[c - 'a'] == nullptr) {
+                current->children[c - 'a'] = new TrieNode();
+            }
+            current = current->children[c - 'a'];
+        }
+        current->isEnd = true;
     }
 
-    string shortestRoot(string word, unordered_set<string> dict){
-        for(int len=1; len <= word.size(); len++){
-            string prefix = word.substr(0,len);
-            if(dict.contains(prefix))
-                return prefix;
+    // Find the shortest root of the word in the trie
+    string shortestRoot(string word) {
+        TrieNode* current = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word[i];
+            if (current->children[c - 'a'] == nullptr) {
+                // There is not a corresponding root in the trie
+                return word;
+            }
+            current = current->children[c - 'a'];
+            if (current->isEnd) {
+                return word.substr(0, i + 1);
+            }
         }
+        // There is not a corresponding root in the trie
         return word;
     }
+};
+
+class Solution {
+public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> dict(dictionary.begin(), dictionary.end());
-        vector<string> words = split(sentence,' ');
-        string ans;
-        for(auto &str:words){
-            ans += shortestRoot(str,dict) + ' ';
+        istringstream sStream(sentence);
+
+        Trie dictTrie;
+        for (string word : dictionary) {
+            dictTrie.insert(word);
         }
-        ans.pop_back();
-        return ans;
+
+        // Replace each word in sentence with the corresponding shortest root
+        string word;
+        string newSentence;
+        while (getline(sStream, word, ' ')) {
+            newSentence += dictTrie.shortestRoot(word) + " ";
+        }
+
+        newSentence.pop_back();  // remove extra space
+        return newSentence;
     }
 };
-// Title: Replace Words
+
+
+// class Solution {
+// public:
+//     vector<string> split
+//     string replaceWords(vector<string>& dictionary, string sentence) {
+//         unordered_set<string> dict(dictionary.begin(), dictionary.end());
+//         vector<string> words = sentence.split(" ");
+//         for(auto &str:words)
+//             cout<<str<<endl;
+//         string ans;
+//         return ans;
+//     }
+// };
