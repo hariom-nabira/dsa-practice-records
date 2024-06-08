73443ebe8c463fb0450688d492d2469636f99337
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-08 08:12:23 +0530

    Add Continuous Subarray Sum

---
+++ Continuous_Subarray_Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size(),sum=0;
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(!mp.contains(sum%k))
+                mp[sum%k] = i;
+            else if(i-mp[sum%k]>=2)
+                return true;
+        }
+        return false;
+    }
+};
