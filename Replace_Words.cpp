482053103286edb30b17de6fd6b95541798f6b0d
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-09 07:01:54 +0530

    Add Replace Words

--- Replace_Words.cpp
+++ Replace_Words.cpp
@@ -1,35 +1,3 @@
-class Trie{
-    class TrieNode{
-    public:
-        bool isEnd;
-        vector<TrieNode*> children = vector<TrieNode*>(26);
-    };
-    TrieNode* root;
-public:
-    Trie(){
-        root = new TrieNode();
-    }
-    void insert(string word){
-        TrieNode* temp = root;
-        for(auto &ch:word){
-            if(!temp->children[ch-'a']){
-                temp->children[ch-'a'] = new TrieNode();
-            }
-            temp = temp->children[ch-'a'];
-        }
-        temp->isEnd = true;
-    }
-    string shortestRoot(string word){
-        TrieNode* temp = root;
-        for(int i=0;i<word.size();i++){
-            if(!temp->children[word[i]-'a']) return word;
-            temp = temp->children[word[i]-'a'];
-            if(temp->isEnd) return word.substr(0,i+1);
-        }
-        return word;
-    }
-};
-
 class Solution {
 public:
     vector<string> split(string sentence, char delimiter){
@@ -41,24 +9,22 @@ public:
         return res;
     }
 
-    // string shortestRoot(string word, unordered_set<string> dict){
-    //     for(int len=1; len <= word.size(); len++){
-    //         string prefix = word.substr(0,len);
-    //         if(dict.contains(prefix))
-    //             return prefix;
-    //     }
-    //     return word;
-    // }
+    string shortestRoot(string word, unordered_set<string> dict){
+        for(int len=1; len <= word.size(); len++){
+            string prefix = word.substr(0,len);
+            if(dict.contains(prefix))
+                return prefix;
+        }
+        return word;
+    }
     string replaceWords(vector<string>& dictionary, string sentence) {
-        Trie* dict = new Trie();
-        for(auto & word:dictionary) dict->insert(word);
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
         vector<string> words = split(sentence,' ');
         string ans;
         for(auto &str:words){
-            ans += dict->shortestRoot(str) + ' ';
+            ans += shortestRoot(str,dict) + ' ';
         }
         ans.pop_back();
         return ans;
     }
 };
-// Title: Replace Words
