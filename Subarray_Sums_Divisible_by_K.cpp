70d58d735103f7b379515a8c55bbcaf0e56aa3f6
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-09 06:33:27 +0530

    Add Subarray Sums Divisible by K

--- Subarray_Sums_Divisible_by_K.cpp
+++ Subarray_Sums_Divisible_by_K.cpp
@@ -6,24 +6,10 @@ public:
         mp[0] = 1;
         for(int i=0;i<n;i++){
             sum += nums[i];
-            // ans += mp[sum%k];
-            // ans += (sum >= 0)? mp[sum%k - k] : mp[sum%k + k];
-            
-            // explaination: since we can have negative remainders there are 2 values (at a diff of k) in the remainder range that will add to my ans
-            // example: for k=6 if sum%k = 2, then we must also look for mp[-4]
-
-            //better way : can we somehow convert both into into some same value and only store that
-            //proposal : how about we convert both into the the upper number?
-            // how to do it? add k to the remainder and take modulo k;
-            // for upper number:  (2+6)%6 = 2
-            // for lower number:  (-4+6)%6 = 2
-            int rem = sum%k;
-            int convertedRem = (rem+k)%k;
-            ans += mp[convertedRem];
-            mp[convertedRem]++;
-            // we could have converted both to the lower number by (rem-k)%k
+            ans += mp[sum%k];
+            ans += (sum >= 0)? mp[sum%k - k] : mp[sum%k + k];
+            mp[sum%k]++;
         }
         return ans;
     }
 };
-// Title: Subarray Sums Divisible by K
