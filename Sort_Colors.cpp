f38fe9b986e2e3e5fcedc6159b3136bb1c0f6741
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-13 14:02:47 +0530

    Add Sort Colors

---
+++ Sort_Colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void swap(int &a,int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    void sortColors(vector<int>& nums) {
+        int l=0,r = nums.size()-1,i=0;
+        while(i<=r){
+            if(nums[i] == 0){
+                swap(nums[l],nums[i]);
+                l++;
+                i++;
+            }else if(nums[i] == 2){
+                swap(nums[r],nums[i]);
+                r--;
+            }else{
+                i++;
+            }
+        }
+    }
+};
