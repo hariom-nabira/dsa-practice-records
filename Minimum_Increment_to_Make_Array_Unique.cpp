5d11417243e7c80afc36a88158d1b1fecbadd586
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-14 08:58:01 +0530

    Add Minimum Increment to Make Array Unique

--- Minimum_Increment_to_Make_Array_Unique.cpp
+++ Minimum_Increment_to_Make_Array_Unique.cpp
@@ -3,15 +3,14 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size(),i=0,ans=0,cnt=0;
-        for(int e=0;e<=1e5;e++){
+        for(int e=0;i<n && e<=1e5;e++){
             while(i<n && nums[i]==e){
                 cnt++;
                 i++;
             }
-            cnt = (cnt==0)? 0 : cnt-1;
+            cnt = (!cnt)? 0 : cnt-1;
             ans += cnt;
         }
         return ans;
     }
 };
-// Title: Minimum Increment to Make Array Unique
