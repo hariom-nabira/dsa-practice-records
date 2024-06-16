6560b36fb190682deef23df105a4b3ea17922969
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-17 04:59:31 +0530

    Add Sum of Square Numbers

---
+++ Sum_of_Square_Numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(int n){
+        if((int)sqrt(n)==sqrt(n)) return true;
+        return false;
+    }
+    bool judgeSquareSum(int c) {
+        for(long long i=0; i*i <= c;i++){
+            if(check(c-(i*i))) return true;
+        }
+        return false;
+    }
+};
