3efccfba24dfa47294bc4aaf10f3ad325763c8c1
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-17 04:57:28 +0530

    Add Sum of Square Numbers

--- Sum_of_Square_Numbers.cpp
+++ Sum_of_Square_Numbers.cpp
@@ -5,10 +5,9 @@ public:
         return false;
     }
     bool judgeSquareSum(int c) {
-        for(long long i=0; i*i <= c;i++){
+        for(int i=0; i*i <= c;i++){
             if(check(c-(i*i))) return true;
         }
         return false;
     }
 };
-// Title: Sum of Square Numbers
