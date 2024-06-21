feeafcceb0939901a324b820d777de9e671defb8
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-22 05:13:46 +0530

    Add Count Number of Nice Subarrays

--- Count_Number_of_Nice_Subarrays.cpp
+++ Count_Number_of_Nice_Subarrays.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
-    }
-
-private:
-    int atMost(vector<int>& nums, int k) {
-        int windowSize = 0, subarrays = 0, start = 0;
-
-        for (int end = 0; end < nums.size(); end++) {
-            windowSize += nums[end] % 2;
-            // Find the first index start where the window has exactly k odd
-            // elements.
-            while (windowSize > k) {
-                windowSize -= nums[start] % 2;
-                start++;
-            }
-            // Increment number of subarrays with end - start + 1.
-            subarrays += end - start + 1;
+        int n=nums.size(),ans=0,curr=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]&1) curr++;
+            ans += mp[curr-k];
+            mp[curr]++;
         }
-
-        return subarrays;
+        return ans;
     }
 };
-// Title: Count Number of Nice Subarrays
