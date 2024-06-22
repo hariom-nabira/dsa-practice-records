ee3b6db345aed153d35d4e656327d16518389e17
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-23 02:43:21 +0530

    Add Maximize Total Cost of Alternating Subarrays

--- Maximize_Total_Cost_of_Alternating_Subarrays.cpp
+++ Maximize_Total_Cost_of_Alternating_Subarrays.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     long long maximumTotalCost(vector<int>& nums) {
-        int n = nums.size();
-        long long ans = nums[0];
-        for(int i=1 ;i < (n-1);i++){
-            if(nums[i] >= 0)
-                ans+=nums[i];
-            else{
-                if(nums[i+1]<0){
-                    ans += -1LL * min(nums[i],nums[i+1]);
-                    ans += max(nums[i],nums[i+1]);
-                }else{
-                    ans += abs(nums[i]);
-                    ans += abs(nums[i+1]);
-                }
-                i++;
-            }
+        if(nums.size()==1) return nums[0];
+        long long ans=0;
+        for(auto& ele:nums){
+            ans+=abs(ele);
         }
-        cout<<n;
-        if(n>=2 && nums[n-2]>0) ans += abs(nums[n-1]);
         return ans;
     }
 };
-// Title: Maximize Total Cost of Alternating Subarrays
