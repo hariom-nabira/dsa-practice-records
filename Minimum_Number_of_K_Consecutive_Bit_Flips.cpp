47ef212b5014defef36184a8cbd24193ee353d37
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-26 03:16:17 +0530

    Add Minimum Number of K Consecutive Bit Flips

---
+++ Minimum_Number_of_K_Consecutive_Bit_Flips.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n=nums.size(), ans=0, flipsActive=0;
+        vector<bool> fliped(n);
+        for(int i=0;i<n;i++){
+            if(i>=k && fliped[i-k]) 
+                flipsActive--;
+            if(flipsActive%2 == nums[i]){
+                if(i+k > n) return -1;
+                fliped[i]=true;
+                ans++;
+                flipsActive++;
+            }
+        }
+        return ans;
+    }
+};
