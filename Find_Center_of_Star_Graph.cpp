5fda7cdf37eb47baf35f66b6fd6ae6269e5ac634
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-27 07:24:58 +0530

    Add Find Center of Star Graph

--- Find_Center_of_Star_Graph.cpp
+++ Find_Center_of_Star_Graph.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
-            return edges[0][0];
-        return edges[0][1];
+        int n=edges.size()+1;
+        vector<int> freq(n+1);
+        for(auto edge:edges){
+            if(freq[edge[0]]) return edge[0];
+            freq[edge[0]]++;
+            if(freq[edge[1]]) return edge[1];
+            freq[edge[1]]++;
+        }
+        return -1;
     }
 };
-// Title: Find Center of Star Graph
