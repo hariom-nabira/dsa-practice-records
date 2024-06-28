3a90d52c4815ebd598835d6d05dcb2a6ad465f52
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-29 04:58:09 +0530

    Add All Ancestors of a Node in a Directed Acyclic Graph

---
+++ All_Ancestors_of_a_Node_in_a_Directed_Acyclic_Graph.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adjacencyList(n);
+        vector<vector<int>> ans(n);
+        for(auto &edge : edges){
+            adjacencyList[edge[0]].push_back(edge[1]);
+        }
+        for(int i=0;i<n;i++){
+            markDecendantsDFS(i,adjacencyList,i,ans);
+        }
+        return ans;
+    }
+    void markDecendantsDFS(int ancestor, vector<vector<int>> &adjacencyList, int curr, vector<vector<int>> &ans){
+        for(int child : adjacencyList[curr]){
+            if(ans[child].empty() || ans[child].back()!=ancestor){
+                ans[child].push_back(ancestor);
+                markDecendantsDFS(ancestor, adjacencyList, child, ans);
+            }
+        }
+    }
+};
