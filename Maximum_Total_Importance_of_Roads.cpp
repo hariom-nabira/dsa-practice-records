abd5e73f6cb7303c8d3d95629081004a71468bca
Hariom Nabira <nabira.hariom@gmail.com>
2024-06-28 14:02:02 +0530

    Add Maximum Total Importance of Roads

---
+++ Maximum_Total_Importance_of_Roads.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        long long ans=0;
+        vector<int> freq(n);
+        for(int i=0;i<roads.size();i++){
+            freq[roads[i][0]]++;
+            freq[roads[i][1]]++;
+        }
+        sort(freq.begin(),freq.end());
+        for(int i=0;i<n;i++){
+            ans += 1LL*freq[i]*(i+1);
+        }
+        return ans;
+    }
+};
