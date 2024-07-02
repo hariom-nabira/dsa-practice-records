692069e1388cd6dc4e9c5e09f02597954d141728
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-03 04:53:09 +0530

    Add Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=4) return 0;
+        sort(nums.begin(),nums.end());
+        int mn=INT_MAX;
+        for(int i=0;i<=3;i++){
+            mn = min(mn,nums[n-4+i]-nums[i]);
+        }
+        return mn;
+    }
+};
