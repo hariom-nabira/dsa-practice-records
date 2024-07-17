713b6731236ce61f27fea9a32a27638a3ee8d16f
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-17 06:57:28 +0530

    Add Palindrome Partitioning

---
+++ Palindrome_Partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string &s){
+        int start=0, end=s.size()-1;
+        while(start<=end){
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> curr;
+        helper(s, 0, ans, curr);
+        return ans;
+    }
+    void helper(string &s, int index, vector<vector<string>> & ans, vector<string> &curr){
+        if(index==s.size()){
+            ans.push_back(curr);
+        }
+        for(int i=1; i <= s.size()-index; i++){
+            string substring = s.substr(index,i);
+            if(isPalindrome(substring)){
+                curr.push_back(s.substr(index,i));
+                helper(s, index+i, ans, curr);
+                curr.pop_back();
+            }
+        }
+    }
+};
