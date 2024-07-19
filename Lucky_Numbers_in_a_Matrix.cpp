2aebb5318c0713d1216f29781434ade9c7d74b2d
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-20 03:24:36 +0530

    Add Lucky Numbers in a Matrix

---
+++ Lucky_Numbers_in_a_Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size();
+        vector<int> rowMin(n,INT_MAX);
+        vector<int> colMax(m,INT_MIN);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                rowMin[i] = min(rowMin[i],matrix[i][j]);
+                colMax[j] = max(colMax[j],matrix[i][j]);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rowMin[i]==colMax[j]) ans.push_back(rowMin[i]);
+            }
+        }
+        return ans;
+    }
+};
