a19bd68a12a113ecde2ddde539bf87a521d8745d
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-20 04:39:17 +0530

    Add Find Valid Matrix Given Row and Column Sums

---
+++ Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n=rowSum.size(), m=colSum.size();
+        vector<vector<int>> mat(n, vector<int>(m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int mn = min(rowSum[i],colSum[j]);
+                mat[i][j] = mn;
+                rowSum[i] -= mn;
+                colSum[j] -= mn;
+            }
+        }
+        return mat;
+    }
+};
