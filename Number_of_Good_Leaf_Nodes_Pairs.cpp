362ca1268ae6867ae6dc8c89a27cb48e3d5a7aae
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-19 15:59:35 +0530

    Add Number of Good Leaf Nodes Pairs

--- Number_of_Good_Leaf_Nodes_Pairs.cpp
+++ Number_of_Good_Leaf_Nodes_Pairs.cpp
@@ -10,40 +10,60 @@
  * };
  */
 class Solution {
+private:
+    void traverseTree(TreeNode* currNode, TreeNode* prevNode,
+                      unordered_map<TreeNode*, vector<TreeNode*>>& graph,
+                      unordered_set<TreeNode*>& leafNodes) {
+        if (!currNode) {
+            return;
+        }
+        if (!currNode->left && !currNode->right) {
+            leafNodes.insert(currNode);
+        }
+        if (prevNode) {
+            graph[prevNode].push_back(currNode);
+            graph[currNode].push_back(prevNode);
+        }
+        traverseTree(currNode->left, currNode, graph, leafNodes);
+        traverseTree(currNode->right, currNode, graph, leafNodes);
+    }
+
 public:
     int countPairs(TreeNode* root, int distance) {
-        // return helper(root,distance) [11];
-        vector<int> ans = helper(root,distance);
-        return ans[11];
-    }
-    // as per the constraints distance<=10 
-    // therefore we store number of leafNodes at a dist of 0-10 (at respective indices)
-    // and at the 11th indice we store the ans
+        unordered_map<TreeNode*, vector<TreeNode*>> graph;
+        unordered_set<TreeNode*> leafNodes;
 
-    vector<int> helper(TreeNode* root, int& distance){
-        if(!root) return vector<int>(12);
-        if(!root->left && !root->right){
-            vector<int> curr(12);
-            curr[0] = 1;
-            return curr;
-        }
-        vector<int> left = helper(root->left,distance);
-        vector<int> right = helper(root->right,distance);
-        vector<int> curr(12);
+        traverseTree(root, nullptr, graph, leafNodes);
 
-        for(int d1=0; d1<=10; d1++){
-            for(int d2=0;d2<=10;d2++){
-                if(2+d1+d2 <= distance){
-                    curr[11] += left[d1] * right[d2];
+        int ans = 0;
+
+        for (auto leaf : leafNodes) {
+            queue<TreeNode*> bfsQueue;
+            unordered_set<TreeNode*> seen;
+            bfsQueue.push(leaf);
+            seen.insert(leaf);
+
+            // Go through all nodes that are within the given distance of
+            // the current leaf node
+            for (int i = 0; i <= distance; i++) {
+                int size = bfsQueue.size();
+                for (int j = 0; j < size; j++) {
+                    TreeNode* currNode = bfsQueue.front();
+                    bfsQueue.pop();
+                    if (leafNodes.count(currNode) && currNode != leaf) {
+                        ans++;
+                    }
+                    // if (graph.count(currNode)) {
+                        for (auto neighbor : graph[currNode]) {
+                            if (!seen.count(neighbor)) {
+                                bfsQueue.push(neighbor);
+                                seen.insert(neighbor);
+                            }
+                        }
+                    // }
                 }
             }
         }
-
-        for(int i=0; i<=9; i++){
-            curr[i+1] = left[i] + right[i];
-        }
-        curr[11] += left[11]+right[11];
-        return curr;
+        return ans / 2;
     }
 };
-// Title: Number of Good Leaf Nodes Pairs
