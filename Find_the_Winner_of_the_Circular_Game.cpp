d605fe9eb88a3b02f5f6e18d44e0ab6264e59d37
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-20 23:48:19 +0530

    Add Find the Winner of the Circular Game

--- Find_the_Winner_of_the_Circular_Game.cpp
+++ Find_the_Winner_of_the_Circular_Game.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        return helper(n,k)+1;
-    }
-    int helper(int n,int k){
-        if(n==1) return 0;
-        return (helper(n-1,k)+k)%n;
+        queue<int> q;
+        for(int i=1;i<=n;i++) q.push(i);
+        while(q.size()>1){
+            for(int i=1;i<k;i++){
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
+        }
+        return q.front();
     }
 };
-// Title: Find the Winner of the Circular Game
