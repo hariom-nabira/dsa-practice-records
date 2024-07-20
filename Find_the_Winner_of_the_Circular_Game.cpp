4a885f3b47324761a2b707f013d0081da6b8db71
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-21 00:17:07 +0530

    Add Find the Winner of the Circular Game

--- Find_the_Winner_of_the_Circular_Game.cpp
+++ Find_the_Winner_of_the_Circular_Game.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        int index=0;  //when size=1, winner is at index=0
-        for(int size=2;size<=n;size++){
-            // the key logic here is that 
-            // the round with x players eliminates 1 player and leads to a round with x-1 players
-            // observe that the new indices of these x-1 players gets shifed by -k (%x)
-            // building upwards from size: 1 to n
-            //we start with ansIndex=0 and with every step up we shift index by +k(%currSize)
-            index = (index+k)%size;
-        }
-        return index + 1;
+        return helper(n,k)+1;
+    }
+    int helper(int n,int k){
+        if(n==1) return 0;
+        return (helper(n-1,k)+k)%n;
     }
 };
-// Title: Find the Winner of the Circular Game
