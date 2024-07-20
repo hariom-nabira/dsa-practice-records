a9f09d540f776d4c81da5aa22b713b49831abd23
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-20 10:56:45 +0530

    Add Letter Combinations of a Phone Number

---
+++ Letter_Combinations_of_a_Phone_Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    unordered_map<char, string> mp = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+        {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+        {'8', "tuv"}, {'9', "wxyz"}
+    };
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        helper(0,digits, "", ans);
+        return ans;
+    }
+    void helper(int i, string &digits, string curr, vector<string> &ans){
+        if(i==digits.size() && !curr.empty()){
+            ans.push_back(curr);
+            return;
+        }
+        for(char ch : mp[digits[i]]){
+            helper(i+1,digits,curr+ch,ans);
+        }
+    }
+};
