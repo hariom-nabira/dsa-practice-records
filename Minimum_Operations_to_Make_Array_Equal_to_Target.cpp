e0e7f516c3be1d6d8adbd0760b4a79da66b90b8d
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-21 03:13:09 +0530

    Add Minimum Operations to Make Array Equal to Target

--- Minimum_Operations_to_Make_Array_Equal_to_Target.cpp
+++ Minimum_Operations_to_Make_Array_Equal_to_Target.cpp
@@ -7,7 +7,7 @@ public:
         long long mn = abs(nums[0]);
         bool dec = false;
         for(int i=1;i<n;i++){
-            if((nums[i]<0 && nums[i-1]<0) || (nums[i]>0 && nums[i-1]>0)){
+            if((nums[i]<0 && nums[i-1]<0) || (nums[i]>=0 && nums[i-1]>=0)){
                 if(abs(nums[i]) >= abs(nums[i-1]) && !dec){
                     ans += abs(nums[i]) - abs(nums[i-1]);
                 }else if(abs(nums[i]) >= abs(nums[i-1]) && !dec){
@@ -24,4 +24,3 @@ public:
         return ans;
     }
 };
-// Title: Minimum Operations to Make Array Equal to Target
