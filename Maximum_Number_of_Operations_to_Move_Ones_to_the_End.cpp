24686baf0214732eae1443bed524bd567493886e
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-21 02:42:38 +0530

    Add Maximum Number of Operations to Move Ones to the End

---
+++ Maximum_Number_of_Operations_to_Move_Ones_to_the_End.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int z=0,ans=0;
+        for(int i=s.size()-1;i>=0;i--){
+            int j=i;
+            while(j>=0 && s[j]=='0'){
+                j--;
+            }
+            if(j!=i){
+                z++;
+                i = j+1;
+            }
+            else ans += z;
+        }
+        return ans;
+    }
+};
