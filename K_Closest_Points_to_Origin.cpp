622be720230b92eab12c6655656f21ce6837ce38
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-23 07:12:48 +0530

    Add K Closest Points to Origin

---
+++ K_Closest_Points_to_Origin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        priority_queue<pair<float,int>> q;
+        for(int i=0;i<n;i++){
+            q.push({sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]),i});
+            if(q.size()>k)
+                q.pop();
+        }
+        vector<vector<int>> ans;
+        while(!q.empty()){
+            ans.push_back(points[q.top().second]);
+            q.pop();
+        }
+        return ans;
+    }
+};
