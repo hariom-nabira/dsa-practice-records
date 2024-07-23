2d54c300af540bc0838bd44a45e73fb9f566e4d7
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-24 05:18:59 +0530

    Add Sort Array by Increasing Frequency

---
+++ Sort_Array_by_Increasing_Frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> freq;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        sort(nums.begin(),nums.end(), [&](const int &a, const int &b){
+            if(freq[a]==freq[b]){
+                return a>b;
+            }
+            return freq[a]<freq[b];
+        });
+        return nums;
+    }
+};
