df873956d23c51173992df14760cdca0837735c7
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-25 05:22:28 +0530

    Add Valid Palindrome II

---
+++ Valid_Palindrome_II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left=0, right = s.size()-1, k=1;
+        while(left<=right){
+            if(s[left]!=s[right]){
+                // if(helper(s,left,right-1)|| helper(s,left+1,right)){
+                //     return true;
+                // }else{
+                //     return false;
+                // }
+                if(!k) return false;
+                if(s[left+1]==s[right]) left++;
+                else if(s[right-1]==s[left]) right--;
+                else return false;
+                k--;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool helper(string s,int left, int right){
+        while(left<=right){
+            if(s[left]!=s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
