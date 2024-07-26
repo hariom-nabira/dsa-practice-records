022b31805ef2f8688fa9c44804caaa3eca46c1c6
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-26 06:53:42 +0530

    Add Range Sum of BST

---
+++ Range_Sum_of_BST.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int left, int right) {
+        if(!root) return 0;
+        int sum = 0;
+        if(root->val >= left && root->val <= right) sum = root->val;
+        return sum + rangeSumBST(root->left,left,right) + rangeSumBST(root->right,left,right);
+    }
+};
