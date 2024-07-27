ca4139be7b256c076ac8b4499a20ffcf3aeff645
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-28 03:03:48 +0530

    Add Find the Count of Numbers Which Are Not Special

---
+++ Find_the_Count_of_Numbers_Which_Are_Not_Special.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n==1) return false;
+        for(int i=2;i*i <= n;i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+    int nonSpecialCount(int l, int r) {
+        int start = sqrt(l), count=0;
+        if(start*start < l) start++;
+        while(start*start <= r){
+            if(isPrime(start)) count++;
+            start++;
+        }
+        return r-l+1-count;
+    }
+};
