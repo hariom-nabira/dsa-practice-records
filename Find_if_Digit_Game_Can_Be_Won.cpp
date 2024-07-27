ad95675eb552e3c4726bfe3866058d0c112e81c4
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-28 02:48:29 +0530

    Add Find if Digit Game Can Be Won

---
+++ Find_if_Digit_Game_Can_Be_Won.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int s=0,d=0;
+        for(auto e:nums){
+            if(e<10){
+                s+=e;
+            }else if(e<100){
+                d+=e;
+            }
+        }
+        return s!=d;
+    }
+};
