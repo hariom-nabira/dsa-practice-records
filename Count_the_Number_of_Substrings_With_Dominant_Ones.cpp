306f25e8547bf77f3765a184603a07d922948d5c
Hariom Nabira <nabira.hariom@gmail.com>
2024-07-28 03:21:47 +0530

    Add Count the Number of Substrings With Dominant Ones

---
+++ Count_the_Number_of_Substrings_With_Dominant_Ones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        vector<int> pre(n+1);
+        for(int i=0; i<n; i++){
+            pre[i+1] = pre[i];
+            if(s[i]=='0') pre[i+1]++; 
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int z = pre[j+1]-pre[i];
+                int o = j-i+1-z;
+                if(o >= z*z) ans++;
+            }
+        }
+        return ans;
+    }
+};
