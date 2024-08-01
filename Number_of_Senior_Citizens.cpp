4a86ef40835518b48fb401274b08cebbc7250856
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-01 07:22:31 +0530

    Add Number of Senior Citizens

---
+++ Number_of_Senior_Citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans=0;
+        for(auto &detail : details){
+            if(detail[11] > '6') ans++;
+            else if(detail[11]=='6' && detail[12]>'0') ans++;
+        }
+        return ans;
+    }
+};
