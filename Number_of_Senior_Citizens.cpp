a2f02b34bf7c9707233939b9faacba93fb2b9380
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-01 07:20:46 +0530

    Add Number of Senior Citizens

--- Number_of_Senior_Citizens.cpp
+++ Number_of_Senior_Citizens.cpp
@@ -4,9 +4,7 @@ public:
         int ans=0;
         for(auto &detail : details){
             if(detail[11] > '6') ans++;
-            else if(detail[11]=='6' && detail[12]>'0') ans++;
         }
         return ans;
     }
 };
-// Title: Number of Senior Citizens
