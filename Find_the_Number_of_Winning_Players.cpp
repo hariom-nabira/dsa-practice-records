e6f7aded06d78cf359c78e3331845eb9f2857a21
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-03 14:35:29 +0530

    Add Find the Number of Winning Players

--- Find_the_Number_of_Winning_Players.cpp
+++ Find_the_Number_of_Winning_Players.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int winningPlayerCount(int n, vector<vector<int>>& pick) {
-        vector<vector<int>> v(n,vector<int>(11));
+        vector<vector<int>> v(n,vector<int>(10));
         for(auto &p : pick){
             v[p[0]][p[1]]++;
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<11;j++){
+            for(int j=0;j<10;j++){
                 if(v[i][j] > i){
                     ans++;
                     break;
@@ -17,4 +17,3 @@ public:
         return ans;
     }
 };
-// Title: Find the Number of Winning Players
