3e3d8cb0f6cfb5268dad0bb3827f1c639b436b3b
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-03 14:17:11 +0530

    Add Make Two Arrays Equal by Reversing Subarrays

---
+++ Make_Two_Arrays_Equal_by_Reversing_Subarrays.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        int n=target.size();
+        vector<int> freq(1001);
+        for(int i=0;i<n;i++) freq[arr[i]]++;
+        for(int i=0;i<n;i++) freq[target[i]]--;
+        for(int i=0;i<1001;i++) if(freq[i]!=0) return false;
+        return true;
+    }
+};
