971c9a8bde09cb3b058691d530c640f880673bc2
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-05 13:10:27 +0530

    Add Kth Distinct String in an Array

---
+++ Kth_Distinct_String_in_an_Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int> mp;
+        for(auto str:arr) mp[str]++;
+        for(auto str:arr){
+            if(mp[str]==1){
+                k--;
+                if(!k) return str;
+            }
+        }
+        return "";
+    }
+};
