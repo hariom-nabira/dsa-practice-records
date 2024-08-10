286ddb7946495d207648ae2692ac53f9dd5af8c4
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-11 03:16:30 +0530

    Add Find the Count of Monotonic Pairs I

---
+++ Find_the_Count_of_Monotonic_Pairs_I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countOfPairs(vector<int>& nums) {
+        int ans=0;
+        helper(nums,0,0,nums[0],ans);
+        return ans;
+    }
+    void helper(vector<int> &nums,int ind, int l1, int l2, int &ans){
+        if(ind==nums.size()){
+            ans++;
+            return;
+        }
+        for(int i=l1; i<=nums[ind]; i++){
+            if(nums[ind]-i <= l2)
+                helper(nums,ind+1,i,nums[ind]-i,ans);
+        }
+    }
+};
