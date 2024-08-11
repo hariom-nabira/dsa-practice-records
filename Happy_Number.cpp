06d5ab1b2cfd960a3c305fceb5f1f7e5da2e6598
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-11 08:20:21 +0530

    Add Happy Number

---
+++ Happy_Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfSquares(int n){
+        int ans=0;
+        while(n>0){
+            ans += (n%10) * (n%10);
+            n/=10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> s;
+        while(1){
+            if(s.contains(n)) return false;
+            if(n==1) return true;
+            s.insert(n);
+            n = sumOfSquares(n);
+        }
+    }
+};
