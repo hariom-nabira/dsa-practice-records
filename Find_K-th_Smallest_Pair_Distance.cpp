596041e8042c9d5f737f7f1229d9b4a947e8c34c
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-14 03:15:05 +0530

    Add Find K-th Smallest Pair Distance

---
+++ Find_K-th_Smallest_Pair_Distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    void add(priority_queue<int> &q, int val, int k){
+        if(q.size() < k || q.top() > val){
+            q.push(val);
+            if(q.size()>k){
+                q.pop();
+            }
+        }
+    }
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<int> maxHeap;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                add(maxHeap,abs(nums[i]-nums[j]), k);
+            }
+        }
+        return maxHeap.top();
+    }
+};
