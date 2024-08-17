d279c6f0a36cd6a9d0d1c71c1354862efb386fbf
Hariom Nabira <nabira.hariom@gmail.com>
2024-08-17 15:16:54 +0530

    Add Maximum Value Sum by Placing Three Rooks I

--- Maximum_Value_Sum_by_Placing_Three_Rooks_I.cpp
+++ Maximum_Value_Sum_by_Placing_Three_Rooks_I.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     long long maximumValueSum(vector<vector<int>>& board) {
-        int n = board.size(), m = board[0].size(), count=0;
+        int n = board.size(), m = board[0].size();
         long long ans=INT_MIN, currSum=0;
         vector<int> cols(m);
-        helper(0,board,cols,ans,currSum,count);
+        helper(0,board,cols,ans,currSum);
         return ans;
     }
-    void helper(int i, vector<vector<int>> &board, vector<int> &cols, long long &ans, long long currSum,int count){
+    void helper(int i, vector<vector<int>> &board, vector<int> &cols, long long &ans, long long currSum){
         int n = board.size(), m = board[0].size();
         if(i==n){
-            if(count==3)
-                ans = max(ans,currSum);
+            ans = max(ans,currSum);
             return;
         }
         for(int j=0;j<m;j++){
             if(cols[j]==0){
                 cols[j] = 1;
-                count++;
-                helper(i+1,board,cols,ans, currSum + board[i][j], count);
-                count--;
+                helper(i+1,board,cols,ans, currSum + board[i][j]);
                 cols[j] = 0;
             }
         }
-        helper(i+1,board,cols,ans,currSum,count);
     }
 };
-// Title: Maximum Value Sum by Placing Three Rooks I
