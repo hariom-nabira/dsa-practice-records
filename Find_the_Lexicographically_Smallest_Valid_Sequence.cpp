80ac50358051b728e6bd0e3d8a3ff2cda7080758
Hariom Nabira <nabira.hariom@gmail.com>
2024-09-28 15:39:21 +0530

    Add Find the Lexicographically Smallest Valid Sequence

---
+++ Find_the_Lexicographically_Smallest_Valid_Sequence.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        vector<int> ans;
+        int i1=0,i2=0;
+        bool ghapla = true;
+        helper(i1,i2,word1,word2,ans,ghapla);
+        return ans;
+    }
+    bool helper(int i1, int i2, string word1, string word2, vector<int> &ans, bool ghapla){
+        if(i2 == word2.size()) return true;
+        if(word1[i1]==word2[i2]){
+            ans.push_back(i1);
+            if(helper(i1+1,i2+1,word1,word2,ans,ghapla)){
+                return true;
+            }
+            else{
+                ans.pop_back();
+                return false;
+            }
+        }
+        if(ghapla){
+            ans.push_back(i1);
+            if(helper(i1+1,i2+1,word1,word2,ans,!ghapla)){
+                return true;
+            }
+            else{
+                ans.pop_back();
+            }
+        }
+        int n = word1.size();
+        while(i1 < n && word1[i1]!=word2[i2]){
+            i1++;
+        }
+        if(i1 == n) return false;
+        ans.push_back(i1);
+        if(helper(i1+1,i2+1,word1,word2,ans,ghapla)){
+            return true;
+        }else{
+            ans.pop_back();
+            return false;
+        }
+        // for(int j=i1; j < word1.size(); j++){
+        //     if(word1[j] == word2[i2]){
+        //         ans.push_back(j);
+        //         if(!helper(j+1,i2+1,word1,word2,ans,ghapla)){
+        //             ans.pop_back();
+        //         }else{
+        //             return true;
+        //         }
+        //     }else if(ghapla){
+        //         ans.push_back(j);
+        //         if(helper(j+1,i2+1,word1,word2,ans,!ghapla)){
+        //             return true;
+        //         }
+        //         else{
+        //             ans.pop_back();
+        //         }
+        //     }
+        // }
+        // return false;
+    }
+};
