73aab290d1ffece0f9ef5cc23ae11cb57a6634ad
Hariom Nabira <nabira.hariom@gmail.com>
2024-09-28 14:34:12 +0530

    Add Minimum Element After Replacement With Digit Sum

---
+++ Minimum_Element_After_Replacement_With_Digit_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfDigits(int n){
+        int ans=0;
+        while(n>0){
+            ans += n%10;
+            n /= 10;
+        }
+        return ans;
+    }
+    int minElement(vector<int>& nums) {
+        int mn = INT_MAX;
+        for(auto &e : nums){
+            mn = min(mn, sumOfDigits(e));
+        }
+        return mn;
+    }
+};
