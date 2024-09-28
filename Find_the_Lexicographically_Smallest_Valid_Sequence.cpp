874d9ca5a5826a154594b630ca8844e2127c75aa
Hariom Nabira <nabira.hariom@gmail.com>
2024-09-28 15:20:35 +0530

    Add Find the Lexicographically Smallest Valid Sequence

--- Find_the_Lexicographically_Smallest_Valid_Sequence.cpp
+++ Find_the_Lexicographically_Smallest_Valid_Sequence.cpp
@@ -9,46 +9,25 @@ public:
     }
     bool helper(int i1, int i2, string word1, string word2, vector<int> &ans, bool ghapla){
         if(i2 == word2.size()) return true;
-        if(ghapla){
-            ans.push_back(i1);
-            if(helper(i1+1,i2+1,word1,word2,ans,!ghapla)){
-                return true;
+        
+        for(int j=i1; j < word1.size(); j++){
+            if(word1[j] == word2[i2]){
+                ans.push_back(j);
+                if(!helper(j+1,i2+1,word1,word2,ans,ghapla)){
+                    ans.pop_back();
+                }else{
+                    return true;
+                }
+            }else if(ghapla){
+                ans.push_back(j);
+                if(helper(j+1,i2+1,word1,word2,ans,!ghapla)){
+                    return true;
+                }
+                else{
+                    ans.pop_back();
+                }
             }
-            else{
-                ans.pop_back();
-            }
         }
-        int n = word1.size();
-        while(i1 < n && word1[i1]!=word2[i2]){
-            i1++;
-        }
-        if(i1 == n) return false;
-        ans.push_back(i1);
-        if(helper(i1+1,i2+1,word1,word2,ans,ghapla)){
-            return true;
-        }else{
-            ans.pop_back();
-            return false;
-        }
-        // for(int j=i1; j < word1.size(); j++){
-        //     if(word1[j] == word2[i2]){
-        //         ans.push_back(j);
-        //         if(!helper(j+1,i2+1,word1,word2,ans,ghapla)){
-        //             ans.pop_back();
-        //         }else{
-        //             return true;
-        //         }
-        //     }else if(ghapla){
-        //         ans.push_back(j);
-        //         if(helper(j+1,i2+1,word1,word2,ans,!ghapla)){
-        //             return true;
-        //         }
-        //         else{
-        //             ans.pop_back();
-        //         }
-        //     }
-        // }
-        // return false;
+        return false;
     }
 };
-// Title: Find the Lexicographically Smallest Valid Sequence
