c1ee438e6f473669aae894f165b577706e048133
Hariom Nabira <nabira.hariom@gmail.com>
2024-09-29 02:41:05 +0530

    Add Find the K-th Character in String Game I

--- Find_the_K-th_Character_in_String_Game_I.cpp
+++ Find_the_K-th_Character_in_String_Game_I.cpp
@@ -3,8 +3,6 @@ public:
     char kthCharacter(int k) {
         // int changes = ceil(log2(k)) - 1;
         int changes = 0;
-        if((k & (k-1)) == 0) changes++;
-        // cout<<changes;
         while(k>1){
             k = (k+1) % (int)floor(log2(k));
             changes++;
@@ -12,4 +10,3 @@ public:
         return 'a'+changes;
     }
 };
-// Title: Find the K-th Character in String Game I
