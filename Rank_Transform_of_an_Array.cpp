1be716e715a4ff31b90760b5af58f2d9581e0998
Hariom Nabira <nabira.hariom@gmail.com>
2024-10-02 02:27:41 +0530

    Add Rank Transform of an Array

---
+++ Rank_Transform_of_an_Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n=arr.size();
+        vector<pair<int,int>> a;
+        for(int i=0;i<n;i++){
+            a.push_back({arr[i],i});
+        }
+        sort(a.begin(),a.end());
+        for(int i=0;i<n;i++){
+            cout<<a[i].first<<" "<<a[i].second<<"\n";
+        }
+        int rank=1,i=0;
+        vector<int> ans(n);
+        while(i<n){
+            int val = a[i].first;
+            while(i<n && a[i].first == val){
+                ans[a[i].second] = rank;
+                i++;
+            }
+            rank++;
+        }
+        return ans;
+    }
+};
+        // return {};
+// , [&](pair<int,int> &a, pair<int,int> &b){
+//             return a.first >= b.first;
+//         }
