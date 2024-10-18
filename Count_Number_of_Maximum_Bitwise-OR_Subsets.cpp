b0b34d3507400528931cd5ce6ee77a8ad20edcad
Hariom Nabira <nabira.hariom@gmail.com>
2024-10-18 16:44:35 +0530

    Add Count Number of Maximum Bitwise-OR Subsets

---
+++ Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOr = 0;
+        for(auto &e:nums){
+            maxOr |= e;
+        }
+        return helper(0,0,nums,maxOr);
+    }
+    int helper(int i, int currOr, vector<int> &nums, int &maxOr){
+        if(i==nums.size()) return (currOr==maxOr);
+        return helper(i+1,currOr,nums,maxOr) + helper(i+1,currOr|nums[i],nums,maxOr);
+    }
+};
