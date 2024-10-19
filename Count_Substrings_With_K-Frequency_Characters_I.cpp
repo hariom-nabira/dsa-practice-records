19112eb0aa0659bf7a5f588ba2c6eb6bddbb662b
Hariom Nabira <nabira.hariom@gmail.com>
2024-10-20 02:48:22 +0530

    Add Count Substrings With K-Frequency Characters I

---
+++ Count_Substrings_With_K-Frequency_Characters_I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        int i=0, ans=0, n=s.size();
+        vector<int> freq(26);
+        for(int j=0;j<n;j++){
+            freq[s[j]-'a']++;
+            while(freq[s[j]-'a'] >= k){
+                ans += n-j;
+                freq[s[i++]-'a']--;
+            }
+        }
+        return ans;
+    }
+};
