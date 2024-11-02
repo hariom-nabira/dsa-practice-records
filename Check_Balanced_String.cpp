457a58513b0b5b0973b40a71ea86a481760e3455
Hariom Nabira <nabira.hariom@gmail.com>
2024-11-03 02:33:18 +0530

    Add Check Balanced String

---
+++ Check_Balanced_String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isBalanced(string s) {
+        int sum=0, n=s.size();
+        for(int i=0; i<n; i+=2){
+            sum += s[i]-'0';
+        }
+        for(int i=1;i<n;i+=2){
+            sum -= s[i]-'0';
+        }
+        return (sum==0);
+    }
+};
