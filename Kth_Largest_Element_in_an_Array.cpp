ede0c590c3bdf2a429bd330b2901772205d6c33e
Hariom Nabira <nabira.hariom@gmail.com>
2024-11-05 09:44:00 +0530

    Add Kth Largest Element in an Array

---
+++ Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int partition(vector<int>& arr, int left, int right) {
+        int pivotValue = arr[right];
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (arr[i] > pivotValue){
+                swap(arr[storeIndex], arr[i]);
+                storeIndex++;
+            }
+        }
+        swap(arr[storeIndex], arr[right]);
+        return storeIndex;
+    }
+
+    int quickSelect(vector<int>& arr, int left, int right, int k) {
+        if (left == right) {
+            return arr[left];
+        }
+
+        int randPivot = left + std::rand() % (right - left + 1);
+        swap(arr[randPivot], arr[right]);
+
+        int pivotIndex = partition(arr, left, right);
+        
+        if (pivotIndex == k) {
+            return arr[pivotIndex];
+        } else if (pivotIndex > k) {
+            return quickSelect(arr, left, pivotIndex - 1, k);
+        } else {
+            return quickSelect(arr, pivotIndex + 1, right, k);
+        }
+    }
+
+    int findKthLargest(std::vector<int>& arr, int k) {
+        srand(time(nullptr));
+        int size = arr.size();
+        return quickSelect(arr, 0, size - 1, k - 1);
+    }
+};
