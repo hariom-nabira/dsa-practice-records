1eecc275be2144b8a2114d8b3de98f016aaf4d9f
Hariom Nabira <nabira.hariom@gmail.com>
2024-12-23 16:23:47 +0530

    Add Next Permutation

---
+++ Next_Permutation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                swap(nums[i], nums[i+1]);
+                return;
+            }
+        }
+        for(int i=0; i<n/2; i++){
+            swap(nums[i], nums[n-1-i]);
+        }
+    }
+};
