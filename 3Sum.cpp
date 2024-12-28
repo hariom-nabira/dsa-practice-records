42f699ad4ca0965c6116d48903bafb8c6b5b3b98
Hariom Nabira <nabira.hariom@gmail.com>
2024-12-28 09:05:04 +0530

    Add 3Sum

---
+++ 3Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void twoSum(vector<int> &nums, int start, int target, vector<vector<int>> &ans){
+        for(int i=start; i<(int)nums.size(); i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            if(binary_search(nums.begin()+i+1, nums.end(),target - nums[i])){
+                ans.push_back({-1*target, nums[i], target - nums[i]});
+            }
+        }
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i=0; i<=nums.size()-3; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            twoSum(nums,i+1, -1*nums[i], ans);
+        }
+        return ans;
+    }
+};
