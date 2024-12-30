df1ecb7c9267919bdb6f4e0db0319ae5de7d5af9
Hariom Nabira <nabira.hariom@gmail.com>
2024-12-30 19:48:31 +0530

    Add Vertical Order Traversal of a Binary Tree

--- Vertical_Order_Traversal_of_a_Binary_Tree.cpp
+++ Vertical_Order_Traversal_of_a_Binary_Tree.cpp
@@ -1,29 +1,47 @@
-// a MUCH better choice of data structure used than mine
-// although this soln uses DFS, using map<int, multiset<int,int>> would be a better choice even with a BFS soln such as mine
-
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    public:
-        vector<vector<int>> verticalTraversal(TreeNode* root) 
-        {
-            map<int, multiset<pair<int, int>>> mp; // [x][y, val]
-            traverse(root, 0, 0, mp);
-            vector<vector<int>> res;
-            for(auto& [x, st] : mp)
-            {
-                res.push_back({});
-                for(auto& [y, val] : st) res.back().push_back(val);
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, vector<pair<int,priority_queue<int, vector<int>, greater<int>>>>> mp; // col: {{depth, pq<val>}, {{depth, pq<val>}, {{depth, pq<val>}, {{depth, pq<val>}}
+        queue<pair<TreeNode*, pair<int,int>>> q; // {node, {row,col}}
+        q.push({root, {0,0}});
+        while(!q.empty()){
+            TreeNode* node = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            q.pop();
+            if(!node) continue;
+            if(mp.find(col) == mp.end() || mp[col].back().first < row){
+                priority_queue<int, vector<int>, greater<int>> pq;
+                pq.push(node->val);
+                mp[col].push_back({row,pq});
+            }else{ // if(mp[col].back().first == row) //since we are going level order > wali condition aa hi nhi sakti
+                mp[col].back().second.push(node->val);
             }
-            return res;
+            q.push({node->left, {row+1, col-1}});
+            q.push({node->right, {row+1, col+1}});
         }
-        
-    protected:
-        void traverse(TreeNode* node, int x, int y,  map<int, multiset<pair<int, int>>>& mp)
-        {
-            if(!node) return;
-            mp[x].insert({y, node->val});
-            traverse(node->left, x-1, y+1, mp);
-            traverse(node->right, x+1, y+1, mp);
+        vector<vector<int>> ans;
+        for(auto col: mp){
+            vector<int> temp;
+            for(auto rowPQpair: col.second){
+                while(!rowPQpair.second.empty()){
+                    temp.push_back(rowPQpair.second.top());
+                    rowPQpair.second.pop();
+                }
+            }
+            ans.push_back(temp);
         }
+        return ans;
+    }
 };
-// Title: Vertical Order Traversal of a Binary Tree
